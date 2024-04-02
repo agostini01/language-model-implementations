f1a593e8efad7d357a1254d35d03c108329de6a9
Nicolas Agostini <n.b.agostini@gmail.com>
2024-04-02 16:24:48 +0000

    Implement transformer block

--- pytorch/mygpt/gpt.py
+++ pytorch/mygpt/gpt.py
@@ -1,55 +1,87 @@
 import torch
 import torch.nn as nn
 
-from typing import TypeVar
-TensorType = TypeVar('TensorType')
+from torchtyping import TensorType
 
 
-class MultiHeadAttention(nn.Module):
+class TransformerBlock(nn.Module):
 
     def __init__(self, embedding_dim: int, attention_dim: int, num_heads: int):
         super().__init__()
         torch.manual_seed(0)
 
-        self.heads = nn.ModuleList()
-        for _ in range(num_heads):
-            self.heads.append(self.SingleHeadAttention(
-                embedding_dim, attention_dim//num_heads))
+        self.mha = self.MultiHeadAttention(embedding_dim,
+                                           attention_dim, num_heads)
+        self.ln1 = nn.LayerNorm(embedding_dim)
+        self.ln2 = nn.LayerNorm(embedding_dim)
+        self.ffn = self.MLPNet(embedding_dim)
 
-    def forward(self, embedded: TensorType[float]) -> TensorType[float]:
-        # B, T, Head size >>> B, T, Attention dim
-        outputs = [head(embedded) for head in self.heads]
-        cated = torch.cat(outputs, dim=-1)
-        # return cated
-        return torch.round(cated, decimals=4)
+    def forward(self, x: TensorType[float]) -> TensorType[float]:
+        first_part = x + self.mha(self.ln1(x))
+        second_part = first_part + self.ffn(self.ln2(first_part))
+        # return second_part
+        return torch.round(second_part, decimals=4)
 
-    class SingleHeadAttention(nn.Module):
+    class MultiHeadAttention(nn.Module):
 
-        def __init__(self, embedding_dim: int, attention_dim: int):
+        def __init__(self, embedding_dim: int, attention_dim: int, num_heads: int):
             super().__init__()
             torch.manual_seed(0)
-            self.get_keys = nn.Linear(embedding_dim, attention_dim, bias=False)
-            self.get_queries = nn.Linear(
-                embedding_dim, attention_dim, bias=False)
-            self.get_values = nn.Linear(
-                embedding_dim, attention_dim, bias=False)
+
+            self.heads = nn.ModuleList()
+            for _ in range(num_heads):
+                self.heads.append(self.SingleHeadAttention(
+                    embedding_dim, attention_dim//num_heads))
 
         def forward(self, embedded: TensorType[float]) -> TensorType[float]:
-            # (batch_size, seq_len, attention_dim)
-            keys = self.get_keys(embedded)
-            queries = self.get_queries(embedded)
-            values = self.get_values(embedded)
+            # B, T, Head size >>> B, T, Attention dim
+            outputs = [head(embedded) for head in self.heads]
+            cated = torch.cat(outputs, dim=-1)
+            # return cated
+            return torch.round(cated, decimals=4)
 
-            scores = queries @ torch.transpose(keys, 1, 2)
-            B, T, A = keys.shape
-            scores = scores / (A ** 0.5)
+        class SingleHeadAttention(nn.Module):
 
-            pre_mask = torch.tril(torch.ones(T, T))
-            mask = pre_mask == 0
-            scores = scores.masked_fill(mask, float('-inf'))
-            # (batch_size, seq_len, seq_len)
-            scores = nn.functional.softmax(scores, dim=2)
+            def __init__(self, embedding_dim: int, attention_dim: int):
+                super().__init__()
+                torch.manual_seed(0)
+                self.get_keys = nn.Linear(
+                    embedding_dim, attention_dim, bias=False)
+                self.get_queries = nn.Linear(
+                    embedding_dim, attention_dim, bias=False)
+                self.get_values = nn.Linear(
+                    embedding_dim, attention_dim, bias=False)
 
-            transformed = scores @ values
-            # return transformed
-            return torch.round(transformed, decimals=4)
+            def forward(self, embedded: TensorType[float]) -> TensorType[float]:
+                # (batch_size, seq_len, attention_dim)
+                keys = self.get_keys(embedded)
+                queries = self.get_queries(embedded)
+                values = self.get_values(embedded)
+
+                scores = queries @ torch.transpose(keys, 1, 2)
+                B, T, A = keys.shape
+                scores = scores / (A ** 0.5)
+
+                pre_mask = torch.tril(torch.ones(T, T))
+                mask = pre_mask == 0
+                scores = scores.masked_fill(mask, float('-inf'))
+                # (batch_size, seq_len, seq_len)
+                scores = nn.functional.softmax(scores, dim=2)
+
+                transformed = scores @ values
+                # return transformed
+                return torch.round(transformed, decimals=4)
+
+    class MLPNet(nn.Module):
+
+        def __init__(self, embedding_dim: int):
+            super().__init__()
+            torch.manual_seed(0)
+            self.up_projection = nn.Linear(embedding_dim, embedding_dim * 4)
+            self.relu = nn.ReLU()
+            self.down_projection = nn.Linear(embedding_dim * 4, embedding_dim)
+            self.dropout = nn.Dropout(0.2)  # using p = 0.2
+
+        def forward(self, x: TensorType[float]) -> TensorType[float]:
+            torch.manual_seed(0)
+            return self.dropout(self.down_projection(self.relu(self.up_projection(x))))
--- pytorch/tests/test_gpt.py
+++ pytorch/tests/test_gpt.py
@@ -1,7 +1,7 @@
 import unittest
 import torch
 
-from mygpt import SingleHeadAttention, MultiHeadAttention
+from mygpt import SingleHeadAttention, MultiHeadAttention, TransformerBlock
 
 
 class TestSingleHeadAttention(unittest.TestCase):
@@ -53,6 +53,34 @@ class TestMultiHeadAttention(unittest.TestCase):
         # Check the output shape
         self.assertEqual(output.shape, (self.batch_size,
                          self.seq_len, self.attention_dim))
+
+        # Check the output values are rounded to 4 decimal places
+        rounded_output = torch.round(output, decimals=4)
+        self.assertTrue(torch.all(output.eq(rounded_output)))
+
+
+class TestTransformerBlock(unittest.TestCase):
+
+    def setUp(self):
+        self.embedding_dim = 128
+        self.attention_dim = 64
+        self.num_heads = 8
+        self.batch_size = 32
+        self.seq_len = 10
+        self.transformer_block = TransformerBlock(
+            self.embedding_dim, self.attention_dim, self.num_heads)
+
+    def test_forward(self):
+        # Create a random tensor to represent the embedded input
+        embedded = torch.randn(
+            self.batch_size, self.seq_len, self.embedding_dim)
+
+        # Run the forward pass
+        output = self.transformer_block.forward(embedded)
+
+        # Check the output shape
+        self.assertEqual(output.shape, (self.batch_size,
+                         self.seq_len, self.embedding_dim))
 
         # Check the output values are rounded to 4 decimal places
         rounded_output = torch.round(output, decimals=4)
